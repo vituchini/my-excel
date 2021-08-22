c74be7aa322a3e1600d48e915e4fcfb625d9003a
Victor Petrovych <victor.petrovych@teamvoy.com>
2021-08-22 12:29:17 +0300

    Add eslint

--- src/module.js
+++ src/module.js
@@ -1,7 +1,7 @@
 console.log('module.js')
 
 async function start() {
- return await Promise.resolve('polyfill working')
+  return await Promise.resolve('polyfill working')
 }
 
 start().then(console.log)
